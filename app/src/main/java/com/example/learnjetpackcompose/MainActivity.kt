93a13af57a0bc4f31394a6a30e4c7db0444146f1
coder <soumyodipsutra@gmail.com>
2024-01-03 19:07:33 +0530

    learning modifier in depth

--- app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.learnjetpackcompose.ui.theme.LearnJetpackComposeTheme
 
@@ -20,6 +23,8 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent{
+            LearnJetpackComposeTheme() {
+
 //            greeting(name = "World")
 //            Button(onClick = { println("click") }, content = {
 //              Text(text = "button")
@@ -51,19 +56,24 @@ class MainActivity : ComponentActivity() {
 //               Text(text = "hello")
 //               Text(text = "welcome")
 //            }
-              Row(
-                  modifier = Modifier.fillMaxSize(),
-                  horizontalArrangement = Arrangement.Center,
-                  verticalAlignment = Alignment.Top
-              ) {
-                  Text(text = "hello")
-                  Text(text = "welcome")
-              }
+//              Row(
+//                  modifier = Modifier.fillMaxSize(),
+//                  horizontalArrangement = Arrangement.Center,
+//                  verticalAlignment = Alignment.Top
+//              ) {
+//                  Text(text = "hello")
+//                  Text(text = "welcome")
+//              }
+
+                Box(
+                    Modifier.size(100.dp).
+                background(color = Color.Red, shape = RectangleShape))
+            }
             }
         }
     }
 
-@Composable
-fun greeting(name: String){
-    Text(text = "Welcome to jetpack $name", color = Color.Red, fontSize = 22.sp, fontWeight = FontWeight.Bold)
-}
+//@Composable
+//fun greeting(name: String){
+//    Text(text = "Welcome to jetpack $name", color = Color.Red, fontSize = 22.sp, fontWeight = FontWeight.Bold)
+//}
