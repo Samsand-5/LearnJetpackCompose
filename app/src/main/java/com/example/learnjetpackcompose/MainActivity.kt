e3bf7d265b6695a54ea06c1beb27b15a184886d9
coder <soumyodipsutra@gmail.com>
2024-01-20 10:59:52 +0530

    learning Animate*AsState

--- app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/learnjetpackcompose/MainActivity.kt
@@ -451,7 +451,11 @@ class MainActivity : ComponentActivity() {
                         .background(animateColor)
                         .clickable {
                             scale += 0.1f
-                            color = Color(red = Random.nextInt(255), blue = Random.nextInt(255), green = Random.nextInt(255))
+                            color = Color(
+                                red = Random.nextInt(255),
+                                blue = Random.nextInt(255),
+                                green = Random.nextInt(255)
+                            )
                         }
                     )
                 }
